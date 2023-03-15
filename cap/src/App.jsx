8bf9e2500637ca5d2c086504479f209048acad7b
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-14 22:20:44 -0400

    create form for user input on screenshot attributes

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import APIform from './Components/APIform';
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    width: "",
+    height: ""
+  })
 
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name] : value.trim()
+    }))
+  }
+
+  const onSubmit = () => {
+    
+  }
+  
   return (
     <div className="App">
+      <h1>Build Your Own Screenshot! 📸</h1>
 
+      <APIform 
+        inputs={inputs}
+        handleChange={(handleChange)}
+        onSubmit={onsubmit}
+      />
     </div>
   )
 }
---
+++ cap/src/Components/APIform.jsx
@@ -0,0 +1,41 @@
+const APIform = ({inputs, handleChange, onSubmit}) => {
+    // for(const [key, value] of Object.entries(inputs)){
+    //     console.log(`${key}: ${value}`);
+    // }
+    const inputsInfo = [
+        "Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
+        "Input which image format you would prefer for your screenshot: jpeg, png, or webp",
+        "Input true or false if you would like your website screenshot to not contain any ads",
+        "Choose the width of your screenshot (in pixels)",
+        "Choose the height of your screenshot (in pixels)"
+    ]
+    return (
+        <div>
+            <h2> Select Your Image Attributes: </h2>
+            <form className="form-container">
+                {inputs &&  
+                    Object.entries(inputs).map(([category, value], index) => (
+                        <li className="form" key={index}>
+                            <h2>{category}</h2>
+                            <input
+                                className="textbox" 
+                                type="text"
+                                value={value}
+                                name={category}
+                                onChange={handleChange}
+                                placeholder="Input this attribute..."
+                            />
+                            <br></br>
+                            <br></br>
+                            <p> {inputsInfo[index]}</p>
+                        </li>              
+                ))}
+                <button type="submit" className="button" onClick={onSubmit}>
+                    Take that Pic! 🎞
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default APIform;
